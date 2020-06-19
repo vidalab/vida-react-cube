c9336d49de0d1af1ba3de1ff8fe4a0858bfc604b
Phuoc Do <dnprock@gmail.com>
2020-06-19 14:01:28 -0700

    Simple react vida example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import vizJson from './viz.json'
+import VidaComponent from 'vidajs'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width: "100%", height: "100%"}}>
+      <div style={{width: "100%", height: "100%"}}>
+        <VidaComponent vizData={vizJson} />
+      </div>
     </div>
   );
 }
